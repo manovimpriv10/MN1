ab5688dc9a112f869565efb9d524c8cae37d7490
Никита Манов <nikitamanov49@gmail.com>
2025-10-10 21:55:37 +0300

    добавлены примеры, комментарии и улучшен вывод

--- BinPacking/BinPacking/main.cpp
+++ BinPacking/BinPacking/main.cpp
@@ -1,8 +1,14 @@
 #include "binpackingFuncs.h"
+#include <iostream>
 using namespace std;
 
-int main() {
-    setlocale(LC_ALL,"Russian");
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Демонстрация алгоритма упаковки (First Fit)\n";
+    cout << "Пример: если ввод =\n6 10\n2 5 4 7 1 3\n";
+    cout << "ожидается 3 контейнера.\n\n";
+
     vector<int> items;
     int capacity;
 
@@ -12,5 +18,6 @@ int main() {
     auto bins = firstFitPack(items, capacity);
     printResult(bins);
 
+    cout << "\nСпасибо за использование программы!\n";
     return 0;
 }
